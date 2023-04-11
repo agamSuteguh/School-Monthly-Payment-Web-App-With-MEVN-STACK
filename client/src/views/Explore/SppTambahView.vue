0b29f6cc10e3c25f0f8d3606a530143d41313e66
agam <agamzupper@gmail.com>
2023-04-11 23:48:35 +0700

    color up

--- client/src/views/Explore/SppTambahView.vue
+++ client/src/views/Explore/SppTambahView.vue
@@ -1,5 +1,5 @@
 <template>
-  <body class="antialiased font-sans text-gray-100 bg-gray-900">
+  <body class="antialiased font-sans text-gray-900 bg-gray-900">
     <div class="overflow-hidden bg-gray-900">
       <div v-if="account.level === 1"
         class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20"
